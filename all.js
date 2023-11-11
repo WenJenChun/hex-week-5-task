9ebf2fa622ad0b9d521f6ea1e5fa40645e30e39d
WenJenChun <daydreamsss@hotmail.com>
2023-11-11 14:49:29 +0000

    modify week5 task & add week6 task

--- all.js
+++ all.js
@@ -1,36 +1,4 @@
-let data = [
-    {
-      "id": 0,
-      "name": "肥宅心碎賞櫻3日",
-      "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-      "area": "高雄",
-      "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-      "group": 87,
-      "price": 1400,
-      "rate": 10
-    },
-    {
-      "id": 1,
-      "name": "貓空纜車雙程票",
-      "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台北",
-      "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-      "group": 99,
-      "price": 240,
-      "rate": 2
-    },
-    {
-      "id": 2,
-      "name": "台中谷關溫泉會1日",
-      "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台中",
-      "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-      "group": 20,
-      "price": 1765,
-      "rate": 7
-    }
-  ];
-
+let data =[];
 const ticketCardArea = document.querySelector(".ticketCard-area");
 const regionSearch = document.querySelector(".regionSearch");
 const dataNum = document.querySelector("#searchResult-text");
@@ -42,11 +10,25 @@ const ticketPrice = document.querySelector("#ticketPrice");
 const ticketNum = document.querySelector("#ticketNum");
 const ticketRate = document.querySelector("#ticketRate");
 const ticketDescription = document.querySelector("#ticketDescription");
+const ticketForm = document.querySelector(".addTicket-form");
 
-function init(){
+function getData (){
+  axios.get("https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json")
+  .then(function(res){
+    console.log(res.data.data);
+    data = res.data.data;
+    renderCard(data);
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+}
+getData();
+
+function renderCard(cardInfo){
     let str ="";
-    data.forEach(function(item){
-        const content = `<li class="ticketCard">
+    cardInfo.forEach(function(item){
+        str+= `<li class="ticketCard">
         <div class="ticketCard-img">
           <a href="#">
             <img src=${item.imgUrl} alt="">
@@ -74,88 +56,31 @@ function init(){
           </div>
         </div>
       </li>`;
-      str += content;
     });
     ticketCardArea.innerHTML = str;
-     dataNum.textContent = `本次搜尋共 ${data.length} 筆資料`;
+    dataNum.textContent = `本次搜尋共 ${cardInfo.length} 筆資料`;
 }
 
-init();
 
 regionSearch.addEventListener("change", function(e){
-    let str ="";
-    let dataLength = 0;
-    data.forEach(function(item){
-        if(e.target.value === item.area ){
-            dataLength++;
-            const content = `<li class="ticketCard">
-            <div class="ticketCard-img">
-              <a href="#">
-                <img src=${item.imgUrl} alt="">
-              </a>
-              <div class="ticketCard-region">${item.area}</div>
-              <div class="ticketCard-rank">${item.rate}</div>
-            </div>
-            <div class="ticketCard-content">
-              <div>
-                <h3>
-                  <a href="#" class="ticketCard-name">${item.name}</a>
-                </h3>
-                <p class="ticketCard-description">
-                ${item.description}
-                </p>
-              </div>
-              <div class="ticketCard-info">
-                <p class="ticketCard-num">
-                  <span><i class="fas fa-exclamation-circle"></i></span>
-                  剩下最後 <span id="ticketCard-num">${item.group} </span> 組
-                </p>
-                <p class="ticketCard-price">
-                  TWD <span id="ticketCard-price">$${item.price}</span>
-                </p>
-              </div>
-            </div>
-          </li>`;
-          str += content;
-        } else if (e.target.value === "全部地區"){
-            dataLength = data.length;
-            const content = `<li class="ticketCard">
-            <div class="ticketCard-img">
-              <a href="#">
-                <img src=${item.imgUrl} alt="">
-              </a>
-              <div class="ticketCard-region">${item.area}</div>
-              <div class="ticketCard-rank">${item.rate}</div>
-            </div>
-            <div class="ticketCard-content">
-              <div>
-                <h3>
-                  <a href="#" class="ticketCard-name">${item.name}</a>
-                </h3>
-                <p class="ticketCard-description">
-                ${item.description}
-                </p>
-              </div>
-              <div class="ticketCard-info">
-                <p class="ticketCard-num">
-                  <span><i class="fas fa-exclamation-circle"></i></span>
-                  剩下最後 <span id="ticketCard-num">${item.group} </span> 組
-                </p>
-                <p class="ticketCard-price">
-                  TWD <span id="ticketCard-price">$${item.price}</span>
-                </p>
-              </div>
-            </div>
-          </li>`;
-          str += content;
-        }
-    });
-    dataNum.textContent = `本次搜尋共 ${dataLength} 筆資料`;
-    ticketCardArea.innerHTML = str;
+  if(e.target.value === "全部地區"){
+    renderCard(data);
+    return;
+  }
+  let filterData = []; // for暫存
+
+  data.forEach(function(item){
+      if(e.target.value === item.area ){
+          filterData.push(item);
+      } 
+  });
+  renderCard(filterData);
+  
 });
 
 addTicketBtn.addEventListener("click",function(e){
  const obj = {};
+ obj.id = new Date().getTime();
  obj.name = ticketName.value;
  obj.imgUrl= ticketImgUrl.value;
  obj.area= ticketRegion.value;
@@ -164,12 +89,8 @@ addTicketBtn.addEventListener("click",function(e){
  obj.price= ticketPrice.value;
  obj.rate= ticketRate.value;
  data.push(obj);
- init();
- ticketName.value ="";
- ticketImgUrl.value="";
- ticketRegion.value="";
- ticketDescription.value="";
- ticketNum.value="";
- ticketPrice.value="";
- ticketRate.value="";
+ renderCard(data);
+ ticketForm.reset();
+ regionSearch.value = "全部地區";
+
 });
